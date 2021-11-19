c23568289e6015f6e13a2f9eb74c6607880fb52f
movetoheaven <ichvanrachmawan@outlook.com>
2021-11-19 17:37:33 +0700

    update navbar

---
+++ homepage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="homepagecss.css">
+</head>
+
+<body>
+<div class="navbar">
+  <a href="index.php">Home</a>
+  <a href="portofilio.php">About Us</a>
+  <div class="dropdown">
+    <button class="dropbtn">Portofolio
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Photography</a>
+      <a href="#">Videography</a>
+      <a href="#">UI</a>
+      <a href="#">Web Development</a>
+      <a href="#">Android Development</a>
+    </div>
+  </div> 
+  
+</div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,8 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    
+    <center>
+        <h1>Test</h1>
+
+        <h2>Admin Studio</h2>
+        <h2>Admin@gmail.com</h2>
+        <h2>Based in Yogyakarta</h2>
+
+        <form action="homepage.php" method="post">
+            <input type="submit" class="btn btn-outline-dark" value="Enter"></input>
+        </form>
+    </center>  
 </body>
 </html>

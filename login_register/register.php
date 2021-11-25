3f53574ff752fbd6d4a126e5f49e23fdb5f2431d
movetoheaven <ichvanrachmawan@outlook.com>
2021-11-25 08:58:59 +0700

    Fix

--- login_register/register.php
+++ login_register/register.php
@@ -28,10 +28,10 @@
                 </a>
                 <div class="dropdown-content">
                 <a href="../porto_photography/porto-photo.php">Photography</a>
-                <a href="#">Videography</a>
-                <a href="#">UI</a>
+                <a href="../porto_videography/porto-video.php">Videography</a>
+                <a href="../porto_ui/porto_ui.php">UI</a>
                 <a href="../porto_web/porto-web.php">Web Development</a>
-                <a href="#">Android Development</a>
+                <a href="../porto-android/porto-android.php">Android Development</a>
             </div>
             </li>
         </ul>
